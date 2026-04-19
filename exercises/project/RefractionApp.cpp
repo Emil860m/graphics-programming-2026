37ab0b2cccb9e0c77ca9e54bafee00d8c99365ff
Emil Fenger <fenger.emil@gmail.com>
2026-04-19 13:37:31 +0200

    hbjvhjhvj

--- exercises/project/RefractionApp.cpp
+++ exercises/project/RefractionApp.cpp
@@ -80,17 +80,76 @@ void RefractionApp::Cleanup()
 
 void RefractionApp::InitializeCamera()
 {
+    // Create the main camera
+    std::shared_ptr<Camera> camera = std::make_shared<Camera>();
+    camera->SetViewMatrix(glm::vec3(-1, 1, 1), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    camera->SetPerspectiveProjectionMatrix(1.0f, 1.0f, 0.1f, 100.0f);
 
+    // Create a scene node for the camera
+    std::shared_ptr<SceneCamera> sceneCamera = std::make_shared<SceneCamera>("camera", camera);
+
+    // Add the camera node to the scene
+    m_scene.AddSceneNode(sceneCamera);
+
+    // Set the camera scene node to be controlled by the camera controller
+    m_cameraController.SetCamera(sceneCamera);
 }
 
 void RefractionApp::InitializeLights()
 {
+    // Create a directional light and add it to the scene
+    std::shared_ptr<DirectionalLight> directionalLight = std::make_shared<DirectionalLight>();
+    directionalLight->SetDirection(glm::vec3(0.0f, -1.0f, -0.3f)); // It will be normalized inside the function
+    directionalLight->SetIntensity(3.0f);
+    m_scene.AddSceneNode(std::make_shared<SceneLight>("directional light", directionalLight));
 
+    // Create a point light and add it to the scene
+    //std::shared_ptr<PointLight> pointLight = std::make_shared<PointLight>();
+    //pointLight->SetPosition(glm::vec3(0, 0, 0));
+    //pointLight->SetDistanceAttenuation(glm::vec2(5.0f, 10.0f));
+    //m_scene.AddSceneNode(std::make_shared<SceneLight>("point light", pointLight));
 }
 
 void RefractionApp::InitializeMaterial()
 {
+    // Load and build shader
+    std::vector<const char*> vertexShaderPaths;
+    vertexShaderPaths.push_back("shaders/version330.glsl");
+    vertexShaderPaths.push_back("shaders/default.vert");
+    Shader vertexShader = ShaderLoader(Shader::VertexShader).Load(vertexShaderPaths);
 
+    std::vector<const char*> fragmentShaderPaths;
+    fragmentShaderPaths.push_back("shaders/version330.glsl");
+    fragmentShaderPaths.push_back("shaders/default.frag");
+    Shader fragmentShader = ShaderLoader(Shader::FragmentShader).Load(fragmentShaderPaths);
+
+    std::shared_ptr<ShaderProgram> shaderProgramPtr = std::make_shared<ShaderProgram>();
+    shaderProgramPtr->Build(vertexShader, fragmentShader);
+
+    // Get transform related uniform locations
+    ShaderProgram::Location cameraPositionLocation = shaderProgramPtr->GetUniformLocation("CameraPosition");
+    ShaderProgram::Location worldMatrixLocation = shaderProgramPtr->GetUniformLocation("WorldMatrix");
+    ShaderProgram::Location viewProjMatrixLocation = shaderProgramPtr->GetUniformLocation("ViewProjMatrix");
+
+        // Register shader with renderer
+    m_renderer.RegisterShaderProgram(shaderProgramPtr,
+        [=](const ShaderProgram& shaderProgram, const glm::mat4& worldMatrix, const Camera& camera, bool cameraChanged)
+        {
+            if (cameraChanged)
+            {
+                shaderProgram.SetUniform(cameraPositionLocation, camera.ExtractTranslation());
+                shaderProgram.SetUniform(viewProjMatrixLocation, camera.GetViewProjectionMatrix());
+            }
+            shaderProgram.SetUniform(worldMatrixLocation, worldMatrix);
+        },
+        m_renderer.GetDefaultUpdateLightsFunction(*shaderProgramPtr)
+    );
+
+
+    // Filter out uniforms that are not material properties
+    ShaderUniformCollection::NameSet filteredUniforms;
+    filteredUniforms.insert("WorldMatrix");
+    filteredUniforms.insert("ViewProjMatrix");
 }
 
 void RefractionApp::InitializeModels()
